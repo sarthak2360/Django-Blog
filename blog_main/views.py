210a9c068db5f1ce7421e7fab1b4ff5f31baa504
unknown <thakursarthak23@gmail.com>
2025-08-26 15:42:39 +0530

    posts by category

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from blogs.models import Blog, Category
+
 
 def home(request):
-    return render(request,'home.html')
+    # categories = Category.objects.all() #this work is done by the context processor that we have added for all the web pages 
+    featured_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('updated_at')
+    posts = Blog.objects.filter(is_featured=False, status='Published').order_by('updated_at')
+    context ={
+        # 'categories' : categories, #this work is done by the context processor that we have added for all the web pages 
+        'featured_posts' : featured_posts,
+        'posts' : posts,
+    }
+    return render(request,'home.html',context)
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Blog, Category
 
-# Create your views here.
+def posts_by_category(request,category_id):
+    # fetch the posts that belongs to category with the id category_id
+    posts = Blog.objects.filter(status='Published' , category = category_id)
+    # use try/except when we want to do some custom action if category does not exist
+    try:
+        category = Category.objects.get(pk=category_id)
+    except:
+        # redirect user to home page
+        return redirect('home')
+    
+    #use get_object_or_404 when you want to show 4040 error page when category does not exist
+    # category = get_object_or_404(Category,pk=category_id) #either used try or except block or u8se get object method more convienent
+    context = {
+        'posts': posts,
+        'category_id' : category_id,
+        'category' : category,
+    }
+    return render(request , 'post_by_category.html',context)
+
