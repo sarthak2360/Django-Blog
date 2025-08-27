0f8197f7828e1699beb1737b650e7ceaea771319
unknown <thakursarthak23@gmail.com>
2025-08-27 14:07:46 +0530

    search feature

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from .models import Blog, Category
+from django.db.models import Q
 
 def posts_by_category(request,category_id):
     # fetch the posts that belongs to category with the id category_id
@@ -28,3 +29,12 @@ def blogs(request ,slug):
     }
     return render(request,'blogs.html',context)
 
+def search(request):
+    keyword =request.GET.get('keyword')
+    
+    blogs=Blog.objects.filter(Q(title__icontains=keyword) | Q(short_description__icontains=keyword) | Q(blog_body__icontains=keyword),status='Published')
+    context={
+        'blogs':blogs,
+        'keyword' : keyword,
+    }
+    return render(request,'search.html',context)
