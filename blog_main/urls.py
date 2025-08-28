486d2c58ce64fa770f6eb14f128a3718109b75eb
unknown <thakursarthak23@gmail.com>
2025-08-28 17:17:04 +0530

    CUSTOM DASHBOARD panel and CATEGORIES crud functionality

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -31,4 +31,7 @@ urlpatterns = [
     path('register/',views.register,name='register'),
     path('login/',views.login,name='login'),
     path('logout/',views.logout,name='logout'),
+
+    #dashboards url
+    path('dashboard/',include('dashboards.urls')),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
